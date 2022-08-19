78c772870ba779c356fe21d96d3e0a7019d9991c
Michiel Vrins <michiel_678@hotmail.com>
2022-08-19 10:03:34 +0200

    RNP-80: Update the useResource call to match the SDK function signature
    Also add some prettier fixes

--- src/helpers/useResource.ts
+++ src/helpers/useResource.ts
@@ -73,7 +73,7 @@ function useResources(
       scopes,
       profileId,
     );
-  }, [type, shouldAuthenticate, profileId, currentDetails]);
+  }, [type, shouldAuthenticate, profileId, currentDetails, scopes]);
 
   return {
     loading,
