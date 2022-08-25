f75a78f1d0b56a0f47a92146c3a78aa968e48661
Michiel Vrins <michiel_678@hotmail.com>
2022-08-25 10:58:24 +0200

    RNP-83: Fix bug with incorrect error message in pinflow + refactor
    This commit fixes a bug which would occur during pin change. When you
    provided the wrong pin during pin change, any subsequent errors would
    not get shown and instead show the 'wrong pin' error. Thus the error
    from validatePin would not show.
    
    This commit also refactors part of the usePinFlow to make it a bit more
    developer friendly :^).

--- src/helpers/usePinFlow.ts
+++ src/helpers/usePinFlow.ts
@@ -13,23 +13,7 @@ const usePinFlow = () => {
   const [pinLength, setPinLength] = useState<number | null>(null);
   const {getPinProfile, setPinProfile} = useProfileStorage();
 
-  const provideNewPinKey = (newKey: string) =>
-    onNewPinKey(
-      newKey,
-      pin,
-      setPin,
-      flow,
-      setError,
-      pinLength || 5,
-      isConfirmMode,
-      setConfirmMode,
-      firstPin,
-      setFirstPin,
-    );
-
   const cancelPinFlow = () => onCancelPinFlow(flow);
-
-  // Event Handlers
 
   const setInitialState = useCallback(() => {
     setError(null);
@@ -91,6 +75,62 @@ const usePinFlow = () => {
     };
   }, [handleNotification]);
 
+  const provideNewPinKey = (newKey: string) => {
+    const handleConfirmPin = () => {
+      if (newValue.length === pinLength) {
+        if (firstPin === newValue) {
+          OnewelcomeSdk.submitPinAction(
+            flow,
+            Events.PinAction.ProvidePin,
+            newValue,
+          );
+        } else {
+          handleError('Pins do not match');
+        }
+      }
+    };
+    const handleFirstPin = () => {
+      setFirstPin(newValue);
+      if (newValue.length === pinLength) {
+        OnewelcomeSdk.validatePinWithPolicy(newValue)
+          .then(() => {
+            setConfirmMode(true);
+            setPin('');
+          })
+          .catch(err => {
+            handleError(err.message);
+          });
+      }
+    };
+
+    setError(null);
+    if (newKey === '<' && pin.length > 0) {
+      setPin(pin.substring(0, pin.length - 1));
+      return;
+    }
+    const newValue = pin + newKey;
+    setPin(newValue);
+    switch (flow) {
+      case Events.PinFlow.Authentication:
+        if (newValue.length === pinLength) {
+          OnewelcomeSdk.submitPinAction(
+            flow,
+            Events.PinAction.ProvidePin,
+            newValue,
+          );
+        }
+        break;
+      case Events.PinFlow.Create:
+      case Events.PinFlow.Change:
+        if (isConfirmMode) {
+          handleConfirmPin();
+        } else {
+          handleFirstPin();
+        }
+        break;
+    }
+  };
+
   return {
     flow,
     pin,
@@ -102,75 +142,6 @@ const usePinFlow = () => {
     pinLength,
     userInfo,
   };
-};
-
-const onNewPinKey = (
-  newKey: string,
-  pin: string,
-  setPin: (pin: string) => void,
-  flow: Events.PinFlow,
-  setError: (error: string | null) => void,
-  requiredPinLength: number,
-  isConfirmMode: boolean,
-  setConfirmMode: (mode: boolean) => void,
-  firstPin: string,
-  setFirstPin: (pin: string) => void,
-) => {
-  setError(null);
-  if (newKey === '<' && pin.length > 0) {
-    setPin(pin.substring(0, pin.length - 1));
-    return;
-  }
-  const newValue = pin + newKey;
-  setPin(newValue);
-  switch (flow) {
-    case Events.PinFlow.Authentication:
-      if (newValue.length === requiredPinLength) {
-        OnewelcomeSdk.submitPinAction(
-          flow,
-          Events.PinAction.ProvidePin,
-          newValue,
-        );
-      }
-      break;
-    case Events.PinFlow.Create:
-    case Events.PinFlow.Change:
-      if (isConfirmMode) {
-        handleConfirmPin();
-      } else {
-        handleFirstPin();
-      }
-      break;
-  }
-  function handleConfirmPin() {
-    if (newValue.length === requiredPinLength) {
-      if (firstPin === newValue) {
-        OnewelcomeSdk.submitPinAction(
-          flow,
-          Events.PinAction.ProvidePin,
-          newValue,
-        );
-      } else {
-        setError('Pins do not match');
-        setConfirmMode(false);
-        setPin('');
-      }
-    }
-  }
-  function handleFirstPin() {
-    setFirstPin(newValue);
-    if (newValue.length === requiredPinLength) {
-      OnewelcomeSdk.validatePinWithPolicy(newValue)
-        .then(() => {
-          setConfirmMode(true);
-          setPin('');
-        })
-        .catch(err => {
-          setError(err.message);
-          setPin('');
-        });
-    }
-  }
 };
 
 const onCancelPinFlow = (flow: Events.PinFlow) =>
