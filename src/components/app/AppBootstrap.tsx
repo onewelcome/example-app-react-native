9c60310657c27dbab2095b46e5c86fceb380d9b7
Michiel Vrins <michiel_678@hotmail.com>
2022-10-25 12:17:18 +0200

    RNP-105: Remove RN async storage as it is no longer needed

--- src/components/app/AppBootstrap.tsx
+++ src/components/app/AppBootstrap.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import {Platform, Linking} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import {AuthProvider} from '../../providers/auth.provider';
 import {NavigationContainer} from '@react-navigation/native';
 import App from './App';
-
-const PERSISTENCE_KEY = 'NAVIGATION_STATE_V1';
 
 const AppBootstrap: React.FC<{}> = () => {
   return (
