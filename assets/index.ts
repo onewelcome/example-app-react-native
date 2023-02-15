c7fae70453cbbced576765ea2f28a5e2a52ca41a
Michiel Vrins <michiel_678@hotmail.com>
2023-02-15 14:09:36 +0100

    RNP-86: Update logo

--- assets/index.ts
+++ assets/index.ts
@@ -1,11 +1,9 @@
-import Logo from './logo_onegini.png';
-import OneginiIcon from './icon_onegini.png';
+import Logo from './logo_onewelcome.png';
 import BackAndroid from './icon-back-android.png';
 import BackIos from './icon-back-ios.png';
 
 const Assets = {
   logo: Logo,
-  oneginiIcon: OneginiIcon,
   backAndroid: BackAndroid,
   backIos: BackIos,
 };
